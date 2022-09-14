0087bb3da08005086802b5cec7dd92dd684f11b3
Viktor G. Gumennyy <20798349+sairus7@users.noreply.github.com>
2022-09-15 00:29:23 +0300

    fix read_samples

--- read_ann.cpp
+++ read_ann.cpp
@@ -1,4 +1,4 @@
-// приложение читает разметку из файла формата физионет 
+// приложение читает разметку из файла формата физионет
 // и выводит ее в стандартный вывод в текстовом формате (csv)
 
 #include <iostream>
@@ -9,50 +9,53 @@
 
 using namespace std;
 
-std::string _recordName(const std::string& filepath)
+std::string _recordName(const std::string &filepath)
 {
-  size_t ibeg = filepath.find_last_of('\\');
-  std::string filename = filepath.substr(ibeg+1);
-  ibeg = filename.find_last_of('.');
-  string recname = filename.substr(0, ibeg);
-  return recname;
+    size_t ibeg = filepath.find_last_of('\\');
+    std::string filename = filepath.substr(ibeg + 1);
+    ibeg = filename.find_last_of('.');
+    string recname = filename.substr(0, ibeg);
+    return recname;
 }
-std::string _extensionName(const std::string& filepath)
+std::string _extensionName(const std::string &filepath)
 {
-  size_t ibeg = filepath.find_last_of('.');
-  std::string ext = filepath.substr(ibeg+1);
-  return ext;
+    size_t ibeg = filepath.find_last_of('.');
+    std::string ext = filepath.substr(ibeg + 1);
+    return ext;
 }
-std::string _pathName(const std::string& filepath)
+std::string _pathName(const std::string &filepath)
 {
-  size_t ibeg = filepath.find_last_of('.');
-  string pathname = filepath.substr(0, ibeg);
-  return pathname;
+    size_t ibeg = filepath.find_last_of('.');
+    string pathname = filepath.substr(0, ibeg);
+    return pathname;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
     // char* filename = "I:\\code\\wfdb_wrapper\\data\\I22.atr";
-    char* filename;
-    
+    char *filename;
+
     if (argc > 1)
         filename = argv[1];
     else
         return 1; // ошибка - неправильное кол-во аргументов
+    
+    wfdbquiet(); // чтобы не выдавал сообщения об ошибках!
 
     wfdb_addtopath(filename);
     string extname = _extensionName(filename);
-    char* c_extname = const_cast<char*>(extname.c_str());
+    char *c_extname = const_cast<char *>(extname.c_str());
     string pathname = _pathName(filename);
-    char* c_pathname = const_cast<char*>(pathname.c_str());
+    char *c_pathname = const_cast<char *>(pathname.c_str());
     string recname = _recordName(filename);
-    char* c_recname = const_cast<char*>(recname.c_str());
+    char *c_recname = const_cast<char *>(recname.c_str());
 
     WFDB_Anninfo OpenInfo;
     OpenInfo.name = c_extname; //&*str.begin() - другой способ получить char*
     OpenInfo.stat = WFDB_READ;
 
-    if (annopen(c_recname, &OpenInfo,1) != 0)
+    if (annopen(c_recname, &OpenInfo, 1) != 0)
         return 1; // ошибка - не удалось открыть файл
 
     double freq = (double)sampfreq(c_recname);
@@ -64,7 +67,7 @@ int main(int argc, char* argv[]) {
     while (getann(0, &Annotation) == 0)
     {
         std::cout << Annotation.time;
-        char* symbol = annstr(Annotation.anntyp);
+        char *symbol = annstr(Annotation.anntyp);
         std::cout << ' ' << symbol << ' ';
         if (Annotation.aux != 0)
             std::cout << Annotation.aux;
--- read_samples.cpp
+++ read_samples.cpp
@@ -1,63 +1,86 @@
-// приложение читает сигналы из файла формата физионет 
-// и выводит их в стандартный вывод в бинарном формате 
+// приложение читает сигналы из файла формата физионет
+// и выводит их в стандартный вывод в бинарном формате
 
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <vector>
 
 #include "wfdblib/wfdblib.h"
 #include "wfdblib/wfdb.h"
 
-#define max(a,b)  (((a) > (b)) ? (a) : (b))
-#define min(a,b)  (((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
 using namespace std;
 
-std::string _recordName(const std::string& filepath)
+std::string _recordName(const std::string &filepath)
 {
-  size_t ibeg = filepath.find_last_of('\\');
-  std::string filename = filepath.substr(ibeg+1);
-  ibeg = filename.find_last_of('.');
-  string recname = filename.substr(0, ibeg);
-  return recname;
+    size_t ibeg = filepath.find_last_of('\\');
+    std::string filename = filepath.substr(ibeg + 1);
+    ibeg = filename.find_last_of('.');
+    string recname = filename.substr(0, ibeg);
+    return recname;
 }
-std::string _extensionName(const std::string& filepath)
+std::string _extensionName(const std::string &filepath)
 {
-  size_t ibeg = filepath.find_last_of('.');
-  std::string ext = filepath.substr(ibeg+1);
-  return ext;
+    size_t ibeg = filepath.find_last_of('.');
+    std::string ext = filepath.substr(ibeg + 1);
+    return ext;
 }
-std::string _pathName(const std::string& filepath)
+std::string _pathName(const std::string &filepath)
 {
-  size_t ibeg = filepath.find_last_of('.');
-  string pathname = filepath.substr(0, ibeg);
-  return pathname;
+    size_t ibeg = filepath.find_last_of('.');
+    string pathname = filepath.substr(0, ibeg);
+    return pathname;
 }
 
-int main(int argc, char* argv[]) {
+
+void log_arr(int* arr, int len)
+{
+    for (int i = 0; i < len; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main(int argc, char *argv[])
+{
 
     // char* filename = "I:\\code\\wfdb_wrapper\\data\\I22.dat";
-    char* filename;
-    
+    char *filename;
+
     if (argc > 1)
         filename = argv[1];
     else
         return 1; // ошибка - неправильное кол-во аргументов
 
+    // после создания ссылки ее нельзя привязать к другому объекту,
+    // поэтому привяжем через указатель
+    ostream* this_out = &cout;
+    ofstream outfile;
+    if (argc > 2)
+    {
+        outfile.open(argv[2], ios::out | ios::binary);
+        this_out = &outfile;
+    }
+    ostream &out = *this_out;
+
     // ??? входные аргументы
-    int nChannel = 0;
-    int nFirstPnt = 0;
-    int nPoints = INT32_MAX / 2;
+    // int nChannel = 0;
+    //int nFirstPnt = 0;
+    //int nPoints = INT32_MAX / 2;
     const int c_nPadLengthSec = 60;
+
+    // wfdbquiet(); // чтобы не выдавал сообщения об ошибках!
 
     //	Open file, check number of channels
     wfdb_addtopath(filename);
     string extname = _extensionName(filename);
-    char* c_extname = const_cast<char*>(extname.c_str());
+    char *c_extname = const_cast<char *>(extname.c_str());
     string pathname = _pathName(filename);
-    char* c_pathname = const_cast<char*>(pathname.c_str());
+    char *c_pathname = const_cast<char *>(pathname.c_str());
     string recname = _recordName(filename);
-    char* c_recname = const_cast<char*>(recname.c_str());
+    char *c_recname = const_cast<char *>(recname.c_str());
 
     int nsig = isigopen(c_recname, 0, 0);
     if (!nsig)
@@ -65,15 +88,15 @@ int main(int argc, char* argv[]) {
     vector<WFDB_Siginfo> vsinfo(nsig);
 
     isigopen(c_recname, &vsinfo[0], nsig);
-    if (nChannel < 0 || nChannel >= nsig)
-    {
-      return 1;
-    }
+    // if (nChannel < 0 || nChannel >= nsig)
+    // {
+    //     return 1;
+    // }
 
     double freq = (double)sampfreq(c_recname);
     long pnt = vsinfo.front().nsamp;
     vector<WFDB_Sample> vSmp(nsig);
-    int nPadLen = (int)(c_nPadLengthSec * freq);
+    int nPadLen = 0; //(int)(c_nPadLengthSec * freq);
 
     // vector<vector<int>> ppnt(nsig);
     // for (int ch = 0; ch < nsig; ch++)
@@ -82,45 +105,55 @@ int main(int argc, char* argv[]) {
     // }
 
     //	fill in the points with correct indices
-    int nStart = max(0, nFirstPnt);
-    int nEnd = min(pnt, nFirstPnt + nPoints);
+    int nStart = 0; //max(0, nFirstPnt);
+    int nEnd = pnt; //min(pnt, nFirstPnt + nPoints);
     if (nEnd > nStart)
     {
-        //	Read data
+        // почему-то, если сначала не установить время на последнюю точку, то потом ее нельзя прочитать...
+        if (isigsettime(pnt-1) < 0 || getvec(&vSmp[0]) < 0)
+        {
+            isigclose();
+            return 1;
+        }
+        // устанавливаем на первую точку
         if (isigsettime(nStart) < 0)
         {
-          return 1;
+            isigclose();
+            return 1;
         }
+        // cout << "----------------------" << endl;
+        // cout << nsig << " * " << sizeof(WFDB_Sample) << endl;
         for (int i = nStart; i < nEnd; i++)
         {
+            //isigsettime(i);
             if (getvec(&vSmp[0]) < 0)
             {
-              return 1;
+                isigclose();
+                return 1;
             }
-            // ppnt[i - nFirstPnt] = vSmp[nChannel];
-            cout.write((const char*)&vSmp[0], nsig);
+            out.write((const char *)&vSmp[0], nsig * sizeof(WFDB_Sample));
+            // log_arr((int*)&vSmp[0], nsig);
         }
     }
 
     // pad the points behind the file end
-    
-    int nPadStart = max(pnt, nFirstPnt);
-    int nPadEnd = min(nFirstPnt + nPoints, pnt + nPadLen);
+
+    int nPadStart = pnt; //max(pnt, nFirstPnt);
+    int nPadEnd = pnt + nPadLen; // min(nFirstPnt + nPoints, pnt + nPadLen);
     if (nPadEnd > nPadStart)
     {
-        //	get last value of signal to use for padding
-        if (isigsettime(pnt-1) < 0 || getvec(&vSmp[0]) < 0)
-        {
-          return 1;
-        }
-        unsigned int PadValue = vSmp[nChannel];
-        //	do it
+        // уже прочитали последнее значение из файла - размножим его
         for (int i = nPadStart; i < nPadEnd; i++)
-            //ppnt[i - nFirstPnt] = PadValue;
-            cout.write((const char*)&vSmp[0], nsig); // пишем все точки в стандартный вывод в бинарном виде
+        {
+            out.write((const char *)&vSmp[0], nsig * sizeof(WFDB_Sample)); // пишем все точки в стандартный вывод в бинарном виде
+        }
     }
 
     isigclose();
-    //wfdbquit();
+    // wfdbquit();
+
+    if (outfile.is_open())
+        outfile.close(); // все равно вызовется в декструкторе
+
     return 0;
 }
